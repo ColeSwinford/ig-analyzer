2725c12d2a771ba95c2d3fa4b4970b48b269160a
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2025-11-25 20:53:20 -0500

    - fixed footer length

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => (
   <div className="mt-12 mb-6 text-center space-y-2">
     <p className="text-[#363636] text-xs">Not affiliated with Meta or Instagram</p>
     <p className="text-[#A8A8A8] text-xs">
-        Designed and built by <a href="#" target="_blank" rel="noreferrer" className="text-white hover:text-[#0095F6] transition font-medium">Cole Swinford</a>
+        Designed and built by <a href="https://github.com/ColeSwinford/ig-analyzer" target="_blank" rel="noreferrer" className="text-white hover:text-[#0095F6] transition font-medium">Cole Swinford</a>
     </p>
   </div>
 );
